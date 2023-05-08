77de8c74b92aed0ec5a2a8eec5572465525d86f6
Enis Shallci <75337247+enisshallci@users.noreply.github.com>
2023-05-08 23:12:34 +0200

    Update main.java

--- src/application/main.java
+++ src/application/main.java
@@ -5,20 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import repositories.DatabaseHandler;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class addBook extends Application {
+public class main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/views/addBook.fxml"));
+        Locale currentLocale = Locale.getDefault();
+        Locale locale = new Locale("sq_AL");
+        ResourceBundle boundle = ResourceBundle.getBundle("/properties/Language", locale);
+        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"), boundle);
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+
+        DatabaseHandler.getInstance();       //instantiate databazen nga ketu, per ma shpejt me punu me butonat kur i shtypim.
     }
 
     public static void main(String[] args) {
