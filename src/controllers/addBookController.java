f1f24e5ec1a16872634147ed7a6a8c38bde1099b
arlindnimani1 <100229702+arlindnimani1@users.noreply.github.com>
2023-05-08 20:56:24 +0200

    Update addBookController.java

--- src/controllers/addBookController.java
+++ src/controllers/addBookController.java
@@ -3,12 +3,20 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import models.Book;
 import repositories.DatabaseHandler;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class addBookController implements Initializable {
 
@@ -31,20 +39,124 @@ public class addBookController implements Initializable {
     private TextField title;
 
     @FXML
+    private AnchorPane root;
+
+    private Boolean isInEditMode = Boolean.FALSE;
+
+    @FXML
     void addBook(ActionEvent event) {
+        String bookID = id.getText();
+        String bookAuthor = author.getText();
+        String bookName = title.getText();
+        String bookPublisher = publisher.getText();
+
+        if(isInEditMode){
+            handleEditOperation();
+            return;
+        }
+
+        if (bookID.isEmpty() || bookAuthor.isEmpty() || bookName.isEmpty() || bookPublisher.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Enter in all fields");
+            alert.showAndWait();
+            return;
+        }
+
+
+        // VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+        String query = "INSERT INTO BOOK VALUES ("+
+                "'" + bookID + "'," +
+                "'" + bookName + "'," +
+                "'" + bookAuthor + "'," +
+                "'" + bookPublisher + "'," +
+                "" + "true" + "" +
+                ")";
+        System.out.println(query);
+
+        if (databaseHandler.execAction(query)) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText(null);
+            alert.setContentText("Success");
+            alert.showAndWait();
+            clear();
+        } else {    //Nese nuk ka sukses atehere
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Error");
+            alert.showAndWait();
+            clear();
+        }
 
     }
-
 
     DatabaseHandler databaseHandler;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        databaseHandler = new DatabaseHandler();
+
+        databaseHandler = DatabaseHandler.getInstance();
+        
+        checkData();
+        
     }
+
+    private void checkData() {
+        String query = "SELECT title FROM BOOK";
+        ResultSet rs = databaseHandler.execQuery(query);
+        try {
+            while (rs.next()) {
+                String titlex = rs.getString("title");
+                System.out.println(titlex);
+            }
+        }catch(SQLException ex) {
+            Logger.getLogger(addBookController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void handleEditOperation() {
+        models.Book book = new models.Book(id.getText(),title.getText(),author.getText(),publisher.getText(), true);
+        if(databaseHandler.updateBook(book)){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("SUCCESS");
+            alert.setContentText("Success! Book updated");
+            alert.showAndWait();
+            clear();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Error");
+            alert.setContentText("Failed ! Book can not be updated");
+            alert.showAndWait();
+            clear();
+        }
+    }
+
+    public void inflatedBUI (models.Book book){
+
+        id.setText(book.getBookID());
+        author.setText(book.getAuthor());
+        publisher.setText(book.getPublisher());
+        title.setText(book.getTitle());
+
+        isInEditMode = Boolean.TRUE;
+
+        id.setEditable(false);
+    }
+
 
     @FXML
     void cancel(ActionEvent event) {
-
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.close();
     }
+
+    void clear() {
+        id.setText("");
+        title.setText("");
+        author.setText("");
+        publisher.setText("");
+//        quantity.setText("");
+//        check.setSelected(false);
+    }
+
 }
