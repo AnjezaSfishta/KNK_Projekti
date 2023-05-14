5e6c6ffd9d00c0b4ca7a2aff87e5963c846d4f31
AnjezaSfishta <115582023+AnjezaSfishta@users.noreply.github.com>
2023-05-14 15:52:10 +0200

    Update Member.java

--- src/models/Member.java
+++ src/models/Member.java
@@ -1,1 +1,36 @@
+package models;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Member {
+
+    private final SimpleStringProperty memberID;
+    private final SimpleStringProperty name;
+    private final SimpleStringProperty phone;
+    private final SimpleStringProperty email;
+
+    public Member(String id, String name, String phone, String email) {
+        this.memberID = new SimpleStringProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.phone = new SimpleStringProperty(phone);
+        this.email = new SimpleStringProperty(email);
+    }
+
+    public String getMemberID() {
+        return memberID.get();
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public String getPhone() {
+        return phone.get();
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+}
 
