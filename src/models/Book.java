b4988b07939f6fd069abb5e46fe4ce36c4cc0da4
Enis Shallci <75337247+enisshallci@users.noreply.github.com>
2023-05-15 23:00:06 +0200

    Update Book.java

--- src/models/Book.java
+++ src/models/Book.java
@@ -1,1 +1,48 @@
+package models;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Book {
+
+    private final SimpleStringProperty bookID;
+    private final SimpleStringProperty title;
+    private final SimpleStringProperty author;
+    private final SimpleStringProperty publisher;
+    //private final SimpleIntegerProperty quantity;
+    private final SimpleBooleanProperty availability;
+
+    public Book(String bookId, String title, String author, String publisher, Boolean availability) {
+        this.title = new SimpleStringProperty(title);
+        this.bookID = new SimpleStringProperty(bookId);
+        this.author = new SimpleStringProperty(author);
+        this.publisher = new SimpleStringProperty(publisher);
+        //this.quantity = new SimpleIntegerProperty(quantity);
+        this.availability = new SimpleBooleanProperty(availability);
+    }
+
+    public String getBookID() {
+        return bookID.get();
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public String getAuthor() {
+        return author.get();
+    }
+
+    public String getPublisher() {
+        return publisher.get();
+    }
+
+//    public Integer getQuantity() {
+//        return quantity.get();
+//    }
+
+    public Boolean getAvailability() {
+        return availability.get();
+    }
+
+}
