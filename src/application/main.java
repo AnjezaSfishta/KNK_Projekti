aef1d4f12a2d8f84a94e0f76e79bd16bd0e81170
Anjeza Sfishta <anjeza.sfishta@student.uni-pr.edu>
2023-05-21 14:02:18 +0200

    Update main.java

--- src/application/main.java
+++ src/application/main.java
@@ -5,31 +5,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import models.Pagination;
 import services.DatabaseHandler;
 
 import java.io.IOException;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.ArrayList;
 
 public class main extends Application {
+
+
 
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        Locale currentLocale = Locale.getDefault();
-        Locale locale = new Locale("sq_AL");
-        ResourceBundle boundle = ResourceBundle.getBundle("/properties/Language", locale);
-        Parent root = FXMLLoader.load(getClass().getResource("/View/main.fxml"), boundle);
-        Scene scene = new Scene(root);
 
+        Parent root = FXMLLoader.load(getClass().getResource("/View/logins.fxml"));
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
 
         DatabaseHandler.getInstance();       //instantiate databazen nga ketu, per ma shpejt me punu me butonat kur i shtypim.
+        Pagination pagination = new Pagination(2, 10);
+
+        String sqlQuery = pagination.getSQLQuery();
+        System.out.println("SQL Query: " + sqlQuery);
+
+        ArrayList<Object> sqlParams = pagination.getSQLParams();
+        System.out.println("SQL Parameters: " + sqlParams);
     }
+
 
     public static void main(String[] args) {
         launch(args);
+
+
     }
+
 }
