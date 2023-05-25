1cfd4621b03dd2fa548724c11db8a8e40b4f9a24
Anjeza Sfishta <anjeza.sfishta@student.uni-pr.edu>
2023-05-25 18:54:39 +0200

    Update UserRepository.java

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,127 +1,48 @@
 package repository;
 
-import models.dto.UserDTO;
-import repository.interfaces.UserRepositoryInterface;
+import models.User;
+import services.DatabaseConnection;
+import services.DatabaseHandler;
+import services.PasswordHasher;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class UserRepository implements UserRepositoryInterface {
-
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/admin";
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "12345";
-    private UserDTO user;
+public class UserRepository {
+    private DatabaseHandler databaseHandler;
+    private DatabaseConnection connection;
+    private PreparedStatement pst;
 
     public UserRepository() {
-        // Initialize your repository with any necessary dependencies
+        databaseHandler = new DatabaseHandler();
     }
 
-    @Override
-    public UserDTO getUserById(int id) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "SELECT * FROM users WHERE ID = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, id);
+    public User getUserByUsernameAndPassword(String username, String password) {
+        connection = DatabaseConnection.getConnection();
+        String query = "SELECT * FROM users WHERE username=?";
 
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                int userId = resultSet.getInt("ID");
-                String username = resultSet.getString("USERNAME");
-                String password = resultSet.getString("PASSWORD");
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setString(1, username);
 
-                return new UserDTO(userId, username, password);
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                String saltedPassword = rs.getString("salted_password");
+                String salt = rs.getString("salt");
+
+                if (PasswordHasher.compareSaltedHash(password, salt, saltedPassword)) {
+                    return new User(username, saltedPassword, salt);
+                }
             }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
         }
-        return null;
-    }
-    @Override
-    public UserDTO getUserByUsername(String username) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "SELECT * FROM users WHERE USERNAME = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, username);
 
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                int userId = resultSet.getInt("ID");
-                String dbUsername = resultSet.getString("USERNAME");
-                String password = resultSet.getString("PASSWORD");
-
-                return new UserDTO(userId, dbUsername, password);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
-    @Override
-    public void addUser(UserDTO user) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "INSERT INTO users (USERNAME, PASSWORD) VALUES (?, ?)";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, user.getUsername());
-            statement.setString(2, user.getPassword());
-
-            statement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        return null; // User not found or invalid password
     }
 
-    @Override
-    public void updateUser(UserDTO user) {
-        this.user = user;
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "UPDATE users SET USERNAME = ?, PASSWORD = ? WHERE ID = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, user.getUsername());
-            statement.setString(2, user.getPassword());
-            statement.setInt(3, user.getId());
-
-            statement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    @Override
-    public void deleteUser(int id) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "DELETE FROM users WHERE ID = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, id);
-
-            statement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-    @Override
-    public List<UserDTO> getUsersByFilter(String filter) {
-        List<UserDTO> users = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "SELECT * FROM users WHERE USERNAME LIKE ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, "%" + filter + "%");
-
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int userId = resultSet.getInt("ID");
-                String username = resultSet.getString("USERNAME");
-                String password = resultSet.getString("PASSWORD");
-
-                UserDTO user = new UserDTO(userId, username, password);
-                users.add(user);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return users;
-    }
-
+    // Other methods...
 }
-
